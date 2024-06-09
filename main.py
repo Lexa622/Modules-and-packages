9debc39ebe5318f06747c8f5ed0402a29ef1d8dd
Lexa622 <lexa622@gmail.com>
2024-06-09 12:05:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fake_mfth import divide as dif
+from true_math import divide as dit
+result1 = dif(69, 3)
+result2 = dif(3, 0)
+result3 = dit(49, 7)
+result4 = dit(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
